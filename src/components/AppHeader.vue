d18fa8c0b27dc2165ab6c5963c0ddacd8a4d5c66
Sebastian Velasco <11669998+sebasvelasco353@users.noreply.github.com>
2025-02-07 14:12:13 -0500

    Boilerplate stuff deleted

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,7 +1,19 @@
 <template>
-  <div></div>
+  <nav>
+    HomeDashboard
+    <ul>
+      <li></li>
+      <li></li>
+    </ul>
+  </nav>
 </template>
 
 <script setup></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+nav {
+  width: 100%;
+  height: 6rem;
+  background-color: aqua;
+}
+</style>
