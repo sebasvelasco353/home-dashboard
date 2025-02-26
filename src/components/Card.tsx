10129e3d6383f4f730bd44446d21d05e0f1dfe43
Sebastian Velasco <11669998+sebasvelasco353@users.noreply.github.com>
2025-02-25 20:39:40 -0500

    feat(Card_Component): :construction: Adding the minimize functionality to the card component
    Added isExpanded to the state of the component, this will determine if we show the content or not

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "@/styles/card.module.scss";
 import { CardProps } from "@/common/Card.interface";
 
 const Card = ({ size = "md", children }: CardProps) => {
+  const [isExpanded, setIsExpanded] = useState<boolean>(false);
+
+  function handleMinimizeClick() {
+    setIsExpanded((prev) => !prev);
+  }
+
   return (
     <div className={styles.container}>
       {children}
@@ -12,7 +18,12 @@ const Card = ({ size = "md", children }: CardProps) => {
 };
 
 Card.Title = ({ children }: { children: React.ReactNode }) => {
-  return <h2 className={styles.header}>{children}</h2>;
+  return (
+    <div className={styles.header}>
+      <h2>{children}</h2>
+      <span>icons</span>
+    </div>
+  );
 };
 
 Card.Content = ({ children }: { children: React.ReactNode }) => {
